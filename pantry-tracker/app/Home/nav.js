1e16019daf8072e70c62abafa6b4d6177a3ed17b
DannyFabs <aderemifawoyed@gmail.com>
2024-08-04 21:59:26 -0500

    sign out

--- pantry-tracker/app/Home/nav.js
+++ pantry-tracker/app/Home/nav.js
@@ -2,8 +2,25 @@
 import { Box,Typography,Button, AppBar, IconButton, Toolbar } from "@mui/material";
 import React from 'react';
 import reactDom from "react-dom";
+import { auth } from "../firebase";
+import { useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
+
 
 export default function Nav(){
+
+  const router = useRouter()
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      router.push('/')
+    }).catch((error) => {
+      // An error happened.
+      alert("Working on this.....")
+    });
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -19,7 +36,8 @@ export default function Nav(){
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             Family Grocery Tracker
           </Typography>
-        </Toolbar>
+          <Button color="error" variant="contained" onClick={handleSignOut}>Sign Out</Button>        
+          </Toolbar>
       </AppBar>
     </Box>
   )
